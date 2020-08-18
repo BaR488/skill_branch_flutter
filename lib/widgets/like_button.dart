96e92cfaf2358548dae57eefea6a815f2607010d
karabanov <karabanov@proit.ru>
2020-08-18 22:14:05 +0500

    skillbranch lesson 3-1

---
+++ lib/widgets/like_button.dart
@@ -0,0 +1,67 @@
+import 'package:FlutterGalleryApp/res/app_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LikeButton extends StatefulWidget {
+  final int likeCount;
+  final bool isLiked;
+
+  const LikeButton(this.likeCount, this.isLiked, {Key key});
+
+  @override
+  _LikeButtonState createState() => _LikeButtonState();
+}
+
+class _LikeButtonState extends State<LikeButton> {
+  bool isLiked;
+  int likeCount;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isLiked = widget.isLiked;
+    likeCount = widget.likeCount;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        setState(() {
+          isLiked = !isLiked;
+          if(isLiked) {
+            likeCount++;
+          } else {
+            likeCount--;
+          }
+          print(likeCount);
+        });
+      },
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(this.isLiked ? AppIcons.like_fill : AppIcons.like),
+              SizedBox(
+                width: 4.21,
+              ),
+              Text(likeCount.toString(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto',
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      height: 16 / 14))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
